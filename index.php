837484840af59caa8976bac529351f174a831784
serj053 <srj053@yandex.ru>
2014-12-29 08:31:09 +0300

    6 урок

--- index.php
+++ index.php
@@ -3,3 +3,4 @@
 	header("Location:controller/index.php");
 	
 ?>
+
--- model/News.php
+++ model/News.php
@@ -1,28 +1,29 @@
 <?php 
 	require'Model.php';
-	require'DBConnect.php';
+
 	
 	class News extends Model{
-		static  $table = 'articles';
-		
-		static public function findAll(){
-			return DBConnect::query_all();
-		}
-		
-		static public function findByPk($id){
-			return DBConnect::query_by_id($id);
-		}
-		
-		public function update($id,$title,$content){
-			return DBConnect::query_update($id,$title,$content,$title);
-		}
-		
-		public function insert($title,$content){
-			return DBConnect::query_insert($title,$content);
-		}
+		static protected  $table = 'articles';
+		static protected $columns = array('title','content');
+
+      public function newArticle(){
+          $this->save();
+      }
+
+      public function updateArticle(){
+         // echo '<br>id_srt = '.($id_art);
+          $this->save();
+      }
+
+      public function getAllArticles(){
+          return News::findAll();
+      }
+
+      public function getOneArticle($id){
+          return News::findByPk($id);
+      }
+
 	}
-	
-    //$t = News::findByPk(4);
-	//print_r($t);
+
 
 ?>
